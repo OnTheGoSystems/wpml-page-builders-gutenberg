31ac4e9edea3669ffcfeb5301fa8b0762737d7dc
Igor Gergel <igor.g@onthegosystems.com>
2019-02-20 11:26:07 +0200

    Refactor code to comply WPCS.
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmltm-3081

--- tests/phpunit/tests/test-wpml-gutenberg-string-registration.php
+++ tests/phpunit/tests/test-wpml-gutenberg-string-registration.php
@@ -1,11 +1,20 @@
 <?php
+/**
+ * Test_WPML_Gutenberg_String_Registration class file.
+ *
+ * @package wpml-page-builders-gutenberg
+ */
 
 /**
+ * Class Test_WPML_Gutenberg_String_Registration
+ *
  * @group string-registration
  */
 class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 
 	/**
+	 * Test string registration.
+	 *
 	 * @test
 	 */
 	public function it_registers_strings() {
@@ -28,12 +37,13 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		$blocks['columns block']              = $this->get_block( 'columns', 'some block content' );
 		$blocks['columns block']->innerBlocks = array( $inner_block );
 
-		\WP_Mock::userFunction( 'gutenberg_parse_blocks',
-		                        array(
-			                        'times'  => 1,
-			                        'args'   => array( $post->post_content ),
-			                        'return' => $blocks,
-		                        )
+		\WP_Mock::userFunction(
+			'gutenberg_parse_blocks',
+			array(
+				'times'  => 1,
+				'args'   => array( $post->post_content ),
+				'return' => $blocks,
+			)
 		);
 
 		foreach ( $blocks as $type => $block ) {
@@ -43,27 +53,40 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		\WP_Mock::expectAction( 'wpml_start_string_package_registration', $package );
 		\WP_Mock::expectAction( 'wpml_delete_unused_package_strings', $package );
 
-		$string_factory   = $this->get_string_factory();
+		$string_factory = $this->get_string_factory();
 
 		$subject = $this->get_subject( $string_factory );
 
 		$subject->register_strings( $post, $package );
 	}
 
+	/**
+	 * Check if block is registered.
+	 *
+	 * @param WP_Block_Parser_Block $block   Block.
+	 * @param string                $type    Block type.
+	 * @param array                 $package Translation package.
+	 */
 	private function check_block_is_registered( WP_Block_Parser_Block $block, $type, $package ) {
 		$block_name = isset( $block->blockName ) ? $block->blockName : '';
 
 		$blocks_that_should_be_registered = array( 'normal block', 'columns block', 'inner block' );
 
-		$this->expectAction( 'wpml_register_string',
-		                     array(
-			                     $block->innerHTML,
-			                     md5( $block_name . $block->innerHTML ),
-			                     $package,
-			                     $block_name,
-			                     'VISUAL'
-		                     ),
-		                     in_array( $type, $blocks_that_should_be_registered ) ? 1 : 0
+		$times = 1;
+		if ( $type ) {
+			$times = in_array( $type, $blocks_that_should_be_registered, true ) ? 1 : 0;
+		}
+
+		$this->expectAction(
+			'wpml_register_string',
+			array(
+				$block->innerHTML,
+				md5( $block_name . $block->innerHTML ),
+				$package,
+				$block_name,
+				'VISUAL',
+			),
+			$times
 		);
 
 		if ( isset( $block->innerBlocks ) ) {
@@ -74,6 +97,8 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 	}
 
 	/**
+	 * Test set string location.
+	 *
 	 * @test
 	 * @group wpmlcore-6325
 	 */
@@ -96,21 +121,22 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		$string_name_2 = md5( $blocks['block 2']->blockName . $blocks['block 2']->innerHTML );
 		$string_id_2   = 456;
 
-		\WP_Mock::userFunction( 'gutenberg_parse_blocks',
-		                        array(
-			                        'times'  => 1,
-			                        'args'   => array( $post->post_content ),
-			                        'return' => $blocks,
-		                        )
+		\WP_Mock::userFunction(
+			'gutenberg_parse_blocks',
+			array(
+				'times'  => 1,
+				'args'   => array( $post->post_content ),
+				'return' => $blocks,
+			)
 		);
 
 		\WP_Mock::onFilter( 'wpml_string_id_from_package' )
-			->with( 0, $package, $string_name_1, $blocks['block 1']->innerHTML )
-			->reply( $string_id_1 );
+		        ->with( 0, $package, $string_name_1, $blocks['block 1']->innerHTML )
+		        ->reply( $string_id_1 );
 
 		\WP_Mock::onFilter( 'wpml_string_id_from_package' )
-			->with( 0, $package, $string_name_2, $blocks['block 2']->innerHTML )
-			->reply( $string_id_2 );
+		        ->with( 0, $package, $string_name_2, $blocks['block 2']->innerHTML )
+		        ->reply( $string_id_2 );
 
 		$strings_map = array(
 			array( $string_id_1, $this->get_string( 1 ) ),
@@ -127,6 +153,8 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 	}
 
 	/**
+	 * Test set wrap tag.
+	 *
 	 * @test
 	 * @group wpmltm-3081
 	 */
@@ -149,21 +177,22 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		$string_name_2 = md5( $blocks['block 2']->blockName . $blocks['block 2']->innerHTML );
 		$string_id_2   = 456;
 
-		\WP_Mock::userFunction( 'gutenberg_parse_blocks',
-		                        array(
-			                        'times'  => 1,
-			                        'args'   => array( $post->post_content ),
-			                        'return' => $blocks,
-		                        )
+		\WP_Mock::userFunction(
+			'gutenberg_parse_blocks',
+			array(
+				'times'  => 1,
+				'args'   => array( $post->post_content ),
+				'return' => $blocks,
+			)
 		);
 
 		\WP_Mock::onFilter( 'wpml_string_id_from_package' )
-			->with( 0, $package, $string_name_1, $blocks['block 1']->innerHTML )
-			->reply( $string_id_1 );
+		        ->with( 0, $package, $string_name_1, $blocks['block 1']->innerHTML )
+		        ->reply( $string_id_1 );
 
 		\WP_Mock::onFilter( 'wpml_string_id_from_package' )
-			->with( 0, $package, $string_name_2, $blocks['block 2']->innerHTML )
-			->reply( $string_id_2 );
+		        ->with( 0, $package, $string_name_2, $blocks['block 2']->innerHTML )
+		        ->reply( $string_id_2 );
 
 		$strings_map = array(
 			array( $string_id_1, $this->get_string_with_wrap_tag( 'h1' ) ),
@@ -180,6 +209,8 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 	}
 
 	/**
+	 * Test reuse translation.
+	 *
 	 * @test
 	 */
 	public function it_register_strings_and_reuse_translations() {
@@ -200,20 +231,22 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 
 		$old_string_value = 'some OLD block';
 
-		\WP_Mock::userFunction( 'gutenberg_parse_blocks',
-		                        array(
-			                        'times'  => 1,
-			                        'args'   => array( $post->post_content ),
-			                        'return' => $blocks,
-		                        )
+		\WP_Mock::userFunction(
+			'gutenberg_parse_blocks',
+			array(
+				'times'  => 1,
+				'args'   => array( $post->post_content ),
+				'return' => $blocks,
+			)
 		);
 
 		$original_strings = array(
 			$this->get_string_hash( $old_string_value ) => array( 'value' => $old_string_value ),
-			$this->get_string_hash( $string_value_1 ) => array( 'value' => $string_value_1 ),
+			$this->get_string_hash( $string_value_1 )   => array( 'value' => $string_value_1 ),
 		);
 
 		$current_strings = $original_strings;
+
 		$current_strings[ $this->get_string_hash( $new_string_value ) ] = array( 'value' => $new_string_value );
 
 		$leftover_strings = array(
@@ -221,20 +254,34 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		);
 
 		$string_translation = $this->get_string_translation();
-		$string_translation->method( 'get_package_strings' )
+		$string_translation
+			->method( 'get_package_strings' )
 			->with( $package )
 			->willReturnOnConsecutiveCalls( $original_strings, $current_strings );
 
 		$reuse_translations = $this->get_reuse_translation();
-		$reuse_translations->expects( $this->once() )
-		                   ->method( 'find_and_reuse_translations' )
-		                   ->with( $original_strings, $current_strings, $leftover_strings );
+		$reuse_translations
+			->expects( $this->once() )
+			->method( 'find_and_reuse_translations' )
+			->with( $original_strings, $current_strings, $leftover_strings );
 
 		$subject = $this->get_subject( null, $reuse_translations, $string_translation );
 
 		$subject->register_strings( $post, $package );
 	}
 
+	/**
+	 * Get test subject.
+	 *
+	 * @param WPML_ST_String_Factory|PHPUnit_Framework_MockObject_MockObject     $string_factory     String factory
+	 *                                                                                               object.
+	 * @param WPML_PB_Reuse_Translations|PHPUnit_Framework_MockObject_MockObject $reuse_translations Reuse translation
+	 *                                                                                               object.
+	 * @param WPML_PB_String_Translation|PHPUnit_Framework_MockObject_MockObject $string_translation String translation
+	 *                                                                                               object.
+	 *
+	 * @return WPML_Gutenberg_Strings_Registration
+	 */
 	private function get_subject( $string_factory = null, $reuse_translations = null, $string_translation = null ) {
 		$config_option = \Mockery::mock( 'WPML_Gutenberg_Config_Option' );
 		$config_option->shouldReceive( 'get' )->andReturn( array() );
@@ -243,31 +290,45 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		$reuse_translations = $reuse_translations ? $reuse_translations : $this->get_reuse_translation();
 		$string_translation = $string_translation ? $string_translation : $this->get_string_translation( true );
 
-		return new WPML_Gutenberg_Strings_Registration( $strings_in_block, $string_factory, $reuse_translations, $string_translation );
-	}
-
-	private function get_string_factory() {
-		return $this->getMockBuilder( 'WPML_ST_String_Factory' )
-            ->setMethods( array( 'find_by_id' ) )
-            ->disableOriginalConstructor()->getMock();
+		return new WPML_Gutenberg_Strings_Registration(
+			$strings_in_block,
+			$string_factory,
+			$reuse_translations,
+			$string_translation
+		);
 	}
 
 	/**
-	 * @param int $expected_location
+	 * Get string factory mock.
+	 *
+	 * @return PHPUnit_Framework_MockObject_MockObject
+	 */
+	private function get_string_factory() {
+		return $this->getMockBuilder( 'WPML_ST_String_Factory' )
+		            ->setMethods( array( 'find_by_id' ) )
+		            ->disableOriginalConstructor()->getMock();
+	}
+
+	/**
+	 * Get WPML_ST_String mock with expected location.
+	 *
+	 * @param int $expected_location Expected string location.
 	 *
 	 * @return PHPUnit_Framework_MockObject_MockObject|WPML_ST_String
 	 */
 	private function get_string( $expected_location ) {
 		$string = $this->getMockBuilder( 'WPML_ST_String' )
-            ->setMethods( array( 'set_location' ) )
-            ->disableOriginalConstructor()->getMock();
+		               ->setMethods( array( 'set_location' ) )
+		               ->disableOriginalConstructor()->getMock();
 		$string->expects( $this->once() )->method( 'set_location' )->with( $expected_location );
 
 		return $string;
 	}
 
 	/**
-	 * @param string $expected_wrap_tag
+	 * Get WPML_ST_String mock with expected wrap tag.
+	 *
+	 * @param string $expected_wrap_tag Expected wrap tag.
 	 *
 	 * @return PHPUnit_Framework_MockObject_MockObject|WPML_ST_String
 	 */
@@ -281,16 +342,29 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		return $string;
 	}
 
+	/**
+	 * Get WPML_PB_Reuse_Translations mock.
+	 *
+	 * @return PHPUnit_Framework_MockObject_MockObject
+	 */
 	private function get_reuse_translation() {
 		return $this->getMockBuilder( 'WPML_PB_Reuse_Translations' )
-			->setMethods( array( 'find_and_reuse_translations' ) )
-			->disableOriginalConstructor()->getMock();
+		            ->setMethods( array( 'find_and_reuse_translations' ) )
+		            ->disableOriginalConstructor()->getMock();
 	}
 
+	/**
+	 * Get WPML_PB_String_Translation mock.
+	 *
+	 * @param bool $passthru If pass through is set.
+	 *
+	 * @return PHPUnit_Framework_MockObject_MockObject
+	 */
 	private function get_string_translation( $passthru = false ) {
-		$string_translation = $this->getMockBuilder( 'WPML_PB_String_Translation' )
-            ->setMethods( array( 'get_package_strings', 'get_string_hash' ) )
-            ->disableOriginalConstructor()->getMock();
+		$string_translation = $this
+			->getMockBuilder( 'WPML_PB_String_Translation' )
+			->setMethods( array( 'get_package_strings', 'get_string_hash' ) )
+			->disableOriginalConstructor()->getMock();
 		$string_translation->method( 'get_string_hash' )->willReturnCallback( array( $this, 'get_string_hash' ) );
 
 		if ( $passthru ) {
@@ -300,6 +374,13 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 		return $string_translation;
 	}
 
+	/**
+	 * Get string hash.
+	 *
+	 * @param string $string_value Content of the string.
+	 *
+	 * @return string
+	 */
 	public function get_string_hash( $string_value ) {
 		return md5( $string_value );
 	}
@@ -311,10 +392,14 @@ class Test_WPML_Gutenberg_String_Registration extends OTGS_TestCase {
 	 * @param string $html  Block html.
 	 * @param int    $level Block heading level.
 	 *
-	 * @return \WP_Block_Parser_Block
+	 * @return \WP_Block_Parser_Block|\Mockery\MockInterface
 	 */
 	private function get_block( $name = '', $html = '', $level = 0 ) {
-		/** @var \WP_Block_Parser_Block $block |\Mockery\MockInterface */
+		/**
+		 * Block mock.
+		 *
+		 * @var \WP_Block_Parser_Block|\Mockery\MockInterface $block
+		 */
 		$block = \Mockery::mock( 'WP_Block_Parser_Block' );
 
 		if ( $name ) {
