51beae4b4a13a314bb3763cb578ea628f7a8ab4c
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2019-05-08 18:25:05 -0300

    Automatically add reusable blocks in the translation basket
    As the user has the ability to manually remove items in the translation
    basket, we will not automatically add the blocks in the batch when the
    basket is submitted.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6590

--- src/class-wpml-gutenberg-integration-factory.php
+++ src/class-wpml-gutenberg-integration-factory.php
@@ -38,6 +38,7 @@ class WPML_Gutenberg_Integration_Factory {
 			)
 		);
 
+		$reusable_blocks             = new WPML\PB\Gutenberg\Reusable_Blocks();
 		$reusable_blocks_translation = new WPML\PB\Gutenberg\Reusable_Blocks_Translation( $sitepress );
 
 		$integrations->add(
@@ -48,8 +49,13 @@ class WPML_Gutenberg_Integration_Factory {
 			$integrations->add(
 				new WPML\PB\Gutenberg\Reusable_Blocks_Admin_Integration(
 					new WPML\PB\Gutenberg\Reusable_Blocks_Batch_Handler(
-						new WPML\PB\Gutenberg\Reusable_Blocks(),
+						$reusable_blocks,
 						$reusable_blocks_translation
+					),
+					new WPML\PB\Gutenberg\Reusable_Blocks_Basket_Handler(
+						$reusable_blocks,
+						$reusable_blocks_translation,
+						new \WPML_Translation_Basket( $wpdb )
 					)
 				)
 			);
---
+++ tests/phpunit/tests/reusable-blocks/test-reusable-blocks-basket-handler.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace WPML\PB\Gutenberg;
+
+/**
+ * @group reusable-blocks
+ */
+class Test_Reusable_Blocks_Basket_Handler extends \OTGS_TestCase {
+
+	/**
+	 * @test
+	 * @dataProvider dp_invalid_basket_data
+	 * @group wpmlcore-6590
+	 */
+	public function it_should_NOT_add_blocks_if_missing_array_keys( $data ) {
+		$subject = $this->get_subject();
+		$subject->add_blocks( $data );
+	}
+
+	public function dp_invalid_basket_data() {
+		return [
+			[ 'post' => [], 'translate_from' => 'fr' ],
+			[ 'tr_action' => [], 'translate_from' => 'fr' ],
+			[ 'post' => [], 'tr_action' => [] ],
+		];
+	}
+
+	/**
+	 * @test
+	 * @group wpmlcore-6590
+	 */
+	public function it_should_add_blocks() {
+		$source_lang                 = 'en';
+		$fr                          = 'fr';
+		$de                          = 'de';
+		$post_with_reusable_blocks_1 = 123;
+		$post_with_reusable_blocks_2 = 456;
+		$post_with_no_blocks         = 789;
+		$reusable_block_1            = 1001;
+		$reusable_block_2            = 1002;
+		$reusable_block_3            = 1003;
+		$reusable_block_1_fr         = 1101; // needs update
+		$reusable_block_2_fr         = $reusable_block_2; // not translated
+		$reusable_block_3_fr         = 1103; // not translated
+
+		$data = [
+			'icl_tm_action' => 'add_jobs',
+			'translate_from' => $source_lang,
+			'_icl_nonce_stn_' => '1899935a14',
+			'_wp_http_referer' => '/wp-admin/admin.php?page=wpml-translation-management/menu/main.php&sm=dashboard',
+			'post' => [
+				$post_with_reusable_blocks_1 => [
+					'type' => 'post',
+					'checked' => $post_with_reusable_blocks_1,
+				],
+				$post_with_reusable_blocks_2 => [
+					'type' => 'post',
+					'checked' => $post_with_reusable_blocks_2,
+				],
+				$post_with_no_blocks => [
+					'type' => 'post',
+					'checked' => $post_with_no_blocks,
+				],
+				9999 => [ // Post not selected, missing 'checked'
+					'type' => 'post',
+				],
+				9998 => [ // Not a post
+					'type' => 'string',
+				],
+			],
+			'tr_action' => [
+				$fr => '1',
+				$de => '0',
+			],
+			'iclnonce' => '85df237169',
+		];
+
+		$post_to_reusable_blocks = [
+			[ $post_with_reusable_blocks_1, [ $reusable_block_1, $reusable_block_3 ] ],
+			[ $post_with_reusable_blocks_2, [ $reusable_block_2, $reusable_block_1 ] ],
+		];
+
+		$blocks_mock = $this->get_reusable_blocks();
+		$blocks_mock->method( 'get_ids_from_post' )->willReturnMap( $post_to_reusable_blocks );
+
+		$convert_block_id = [
+			[ $reusable_block_1, 'fr', $reusable_block_1_fr ],
+			[ $reusable_block_2, 'fr', $reusable_block_2_fr ],
+			[ $reusable_block_3, 'fr', $reusable_block_3_fr ],
+		];
+
+		$translation_mock = $this->get_reusable_blocks_translation();
+		$translation_mock->method( 'convert_block_id' )->willReturnMap( $convert_block_id );
+
+		$needs_update = [
+			[ $reusable_block_1_fr, 1 ],
+			[ $reusable_block_3_fr, false ],
+		];
+
+		$status_helper = $this->get_post_status_helper();
+		$status_helper->method( 'needs_update' )->willReturnMap( $needs_update );
+
+		\WP_Mock::userFunction( 'wpml_get_post_status_helper', [
+			'return' => $status_helper,
+		] );
+
+		$basket_portion = [
+			'post' => [
+				$reusable_block_1 => [
+					'from_lang'  => $source_lang,
+					'to_langs'   => [ $fr => 1 ],
+					'auto_added' => true,
+				],
+				$reusable_block_2 => [
+					'from_lang'  => $source_lang,
+					'to_langs'   => [ $fr => 1 ],
+					'auto_added' => true,
+				],
+			],
+			'source_language'	=> $data['translate_from'],
+			'target_languages'	=> array_keys( $data['tr_action'] ),
+		];
+
+		$basket_mock = $this->get_translation_basket();
+		$basket_mock->expects( $this->once() )
+		            ->method( 'update_basket' )
+					->with( $basket_portion );
+
+		$subject = $this->get_subject( $blocks_mock, $translation_mock, $basket_mock );
+
+		$subject->add_blocks( $data );
+	}
+
+	/**
+	 * @test
+	 * @group wpmlcore-6590
+	 * @expectedException \RuntimeException
+	 */
+	public function it_should_throw_an_exception_if_not_an_element_object() {
+		$not_an_element = $this->createMock( 'Not_An_Element' );
+
+		$subject = $this->get_subject();
+		$subject->find_blocks_in_element( $not_an_element );
+	}
+
+	public function get_subject( $blocks = null, $translation = null, $translation_basket = null ) {
+		$blocks             = $blocks ? $blocks : $this->get_reusable_blocks();
+		$translation        = $translation ? $translation : $this->get_reusable_blocks_translation();
+		$translation_basket = $translation_basket ? $translation_basket : $this->get_translation_basket();
+		return new Reusable_Blocks_Basket_Handler( $blocks, $translation, $translation_basket );
+	}
+
+	private function get_reusable_blocks() {
+		return $this->getMockBuilder( Reusable_Blocks::class )
+		            ->setMethods( [ 'get_ids_from_post' ] )
+		            ->disableOriginalConstructor()->getMock();
+	}
+
+	private function get_reusable_blocks_translation() {
+		return $this->getMockBuilder( Reusable_Blocks_Translation::class )
+		            ->setMethods( [ 'convert_block_id' ] )
+		            ->disableOriginalConstructor()->getMock();
+	}
+
+	private function get_translation_basket() {
+		return $this->getMockBuilder( '\WPML_Translation_Basket' )
+		            ->setMethods( [ 'update_basket' ] )
+		            ->disableOriginalConstructor()->getMock();
+	}
+
+	private function get_post_status_helper() {
+		return $this->getMockBuilder( '\WPML_Post_Status' )
+		            ->setMethods( [ 'needs_update' ] )
+		            ->disableOriginalConstructor()->getMock();
+	}
+}
