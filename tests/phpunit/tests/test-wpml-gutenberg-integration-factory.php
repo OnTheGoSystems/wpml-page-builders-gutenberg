02942a723e2291a3021c641f844829fc0f558d71
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2019-05-13 11:09:15 -0300

    Do not load reusable block integration when it's not translatable
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6601

--- tests/phpunit/tests/test-wpml-gutenberg-integration-factory.php
+++ tests/phpunit/tests/test-wpml-gutenberg-integration-factory.php
@@ -14,7 +14,7 @@ class Test_WPML_Gutenberg_Integration_Factory extends OTGS_TestCase {
 	 *
 	 * @param bool $is_admin
 	 */
-	public function it_creates( $is_admin ) {
+	public function it_creates_when_reusable_blocks_are_NOT_translatable( $is_admin ) {
 		global $sitepress, $wpdb;
 
 		\WP_Mock::userFunction( 'is_admin', [
@@ -22,6 +22,45 @@ class Test_WPML_Gutenberg_Integration_Factory extends OTGS_TestCase {
 		] );
 
 		$sitepress = \Mockery::mock( 'SitePress' );
+		$sitepress->shouldReceive( 'is_translated_post_type' )
+		          ->with( WPML\PB\Gutenberg\ReusableBlocks\Translation::POST_TYPE )
+		          ->andReturn( false );
+
+		$wpdb      = \Mockery::mock( 'wpdb' );
+		\Mockery::mock( 'WPML_ST_String_Factory' );
+		\Mockery::mock( 'WPML_PB_Reuse_Translations' );
+		\Mockery::mock( 'WPML_PB_String_Translation' );
+
+		\WP_Mock::userFunction( 'WPML\Container\make', [
+			'args'   => [ '\WPML_Translation_Basket' ],
+			'return' => $this->getMockBuilder( '\WPML_Translation_Basket' )->getMock(),
+		] );
+
+		$factory = new WPML_Gutenberg_Integration_Factory();
+
+		$this->assertInstanceOf( \WPML\PB\Gutenberg\Integration::class, $factory->create() );
+
+		unset( $sitepress );
+	}
+
+	/**
+	 * @test
+	 * @dataProvider dp_is_admin
+	 *
+	 * @param bool $is_admin
+	 */
+	public function it_creates_when_reusable_blocks_are_translatable( $is_admin ) {
+		global $sitepress, $wpdb;
+
+		\WP_Mock::userFunction( 'is_admin', [
+			'return' => $is_admin,
+		] );
+
+		$sitepress = \Mockery::mock( 'SitePress' );
+		$sitepress->shouldReceive( 'is_translated_post_type' )
+		          ->with( WPML\PB\Gutenberg\ReusableBlocks\Translation::POST_TYPE )
+		          ->andReturn( true );
+
 		$wpdb      = \Mockery::mock( 'wpdb' );
 		\Mockery::mock( 'WPML_ST_String_Factory' );
 		\Mockery::mock( 'WPML_PB_Reuse_Translations' );
