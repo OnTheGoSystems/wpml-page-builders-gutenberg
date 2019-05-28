023e0bc25ebc2233127a1350e17a86604a256e00
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2019-05-28 17:25:28 -0300

    Add a notice showing the reusable block jobs automatically created
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6648

---
+++ src/reusable-blocks/class-notice.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace WPML\PB\Gutenberg\ReusableBlocks;
+
+class Notice {
+
+	/** @var \WPML_Notices $notices */
+	private $notices;
+
+	/** @var \WPML_Translation_Job_Factory $job_factory */
+	private $job_factory;
+
+	public function __construct(
+		\WPML_Notices $notices,
+		\WPML_Translation_Job_Factory $job_factory
+	) {
+		$this->notices     = $notices;
+		$this->job_factory = $job_factory;
+	}
+
+	public function addJobsCreatedAutomatically( array $job_ids ) {
+		$job_links = $this->getBlockJobLinks( $job_ids );
+
+		$text = '<p>' . _n(
+			'We automatically created a translation job for the reusable block:',
+			'We automatically created translation jobs for the reusable blocks:',
+			$job_links->count(),
+			'sitepress'
+		) . '</p>';
+
+		$text .= '<ul><li>' . implode( '</li><li>', $job_links->toArray() ) . '</li></ul>';
+
+		$notice = $this->notices->create_notice( 'automatic-jobs', $text, __CLASS__ );
+		$notice->set_flash( true );
+		$notice->set_css_class_types( 'notice-info' );
+		$this->notices->add_notice( $notice );
+	}
+
+	private function getBlockJobLinks( array $job_ids ) {
+		return \collect( $job_ids )->map( function( $job_id ) {
+			return $this->getJobEditLink( $job_id );
+		} )->filter();
+	}
+
+	/**
+	 * @param int $job_id
+	 *
+	 * @return string|null
+	 */
+	private function getJobEditLink( $job_id ) {
+		$job = $this->job_factory->get_translation_job( $job_id );
+
+		if ( ! $job || 'post_wp_block' !== $job->original_post_type ) {
+			return null;
+		}
+
+		$job_edit_url = admin_url( 'admin.php?page='
+		                            . WPML_TM_FOLDER
+		                            . '/menu/translations-queue.php&job_id='
+		                            . $job_id );
+		$job_edit_url = apply_filters( 'icl_job_edit_url', $job_edit_url, $job_id );
+
+		return '<a href="' . $job_edit_url . '" target="_blank">' . $job->title . '</a>';
+	}
+}
---
+++ tests/phpunit/tests/reusable-blocks/test-notice.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace WPML\PB\Gutenberg\ReusableBlocks;
+
+/**
+ * @group reusable-blocks
+ */
+class TestNotice extends \OTGS_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function it_should_add_notice_with_reusable_blocks_job_links() {
+		$job_ids = [
+			'100',
+			'101',
+			'102',
+			'103',
+		];
+
+		$jobs_map = [
+			[ $job_ids[0], $this->getJob( $job_ids[0], 'The post', 'post' ) ],
+			[ $job_ids[1], $this->getJob( $job_ids[1], 'Block 1', 'wp_block' ) ],
+			[ $job_ids[2], $this->getJob( $job_ids[2], 'Block 2', 'wp_block' ) ],
+			[ $job_ids[3], false ],
+		];
+
+		$edit_url_1 = $this->getEditURL( $job_ids[1] );
+		$edit_url_2 = $this->getEditURL( $job_ids[2] );
+
+		\WP_Mock::passthruFunction( 'admin_url' );
+
+		\WP_Mock::onFilter( 'icl_job_edit_url' )
+			->with( $edit_url_1, $job_ids[1] )
+			->reply( $edit_url_1 . '#filtered' );
+
+		\WP_Mock::onFilter( 'icl_job_edit_url' )
+			->with( $edit_url_2, $job_ids[2] )
+			->reply( $edit_url_2 . '#filtered' );
+
+		\WP_Mock::userFunction( '_n', [
+			'args' => [
+				'We automatically created a translation job for the reusable block:',
+				'We automatically created translation jobs for the reusable blocks:',
+				2,
+				'sitepress'
+			],
+			'return' => 'We automatically created translation jobs for the reusable blocks:',
+		] );
+
+		$expected_text = '<p>We automatically created translation jobs for the reusable blocks:</p>';
+		$expected_text .= '<ul><li><a href="' . $edit_url_1 . '#filtered" target="_blank">' . $jobs_map[1][1]->title .'</a></li><li><a href="' . $edit_url_2 . '#filtered" target="_blank">' . $jobs_map[2][1]->title .'</a></li></ul>';
+
+		$notice = $this->getMockBuilder( '\WPML_Notice' )
+			->setMethods( [ 'set_flash', 'set_css_class_types' ] )
+			->disableOriginalConstructor()->getMock();
+		$notice->expects( $this->once() )->method( 'set_flash' )->with( true );
+		$notice->expects( $this->once() )->method( 'set_css_class_types' )->with( 'notice-info' );
+
+		$notices = $this->getMockBuilder( '\WPML_Notices' )
+			->setMethods( [ 'create_notice', 'add_notice' ] )
+			->disableOriginalConstructor()->getMock();
+		$notices->method( 'create_notice' )
+		        ->with( 'automatic-jobs', $expected_text, Notice::class )
+				->willReturn( $notice );
+		$notices->expects( $this->once() )->method( 'add_notice' )->with( $notice );
+
+
+		$job_factory = $this->getMockBuilder( '\WPML_Translation_Job_Factory' )
+			->setMethods( [ 'get_translation_job' ] )
+			->disableOriginalConstructor()->getMock();
+		$job_factory->method( 'get_translation_job' )->willReturnMap( $jobs_map );
+
+		$subject = new Notice( $notices, $job_factory );
+
+		$subject->addJobsCreatedAutomatically( $job_ids );
+	}
+
+	private function getJob( $id, $title, $post_type ) {
+		return (object) [
+			'job_id'             => $id,
+			'title'              => $title,
+			'original_post_type' => 'post_' . $post_type,
+		];
+	}
+
+	private function getEditURL( $job_id ) {
+		return 'admin.php?page=' . WPML_TM_FOLDER . '/menu/translations-queue.php&job_id=' . $job_id;
+	}
+}
