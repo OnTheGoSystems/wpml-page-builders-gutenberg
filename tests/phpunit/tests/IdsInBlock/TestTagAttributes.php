7b9b91a82f9d1d3b4cb903e2ade33dd515f26e75
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-01-16 17:49:57 -0300

    Convert and fallback to original if element is displayed as translated
    If the ID conversion returns a falsy value and the element is displayed
    as translated, we will use the original ID.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wcml-3082

--- tests/phpunit/tests/IdsInBlock/TestTagAttributes.php
+++ tests/phpunit/tests/IdsInBlock/TestTagAttributes.php
@@ -7,18 +7,27 @@ namespace WPML\PB\Gutenberg\ConvertIdsInBlock;
  */
 class TestTagAttributes extends \OTGS_TestCase {
 
+	public function tearDown() {
+		unset( $GLOBALS['sitepress'] );
+		parent::tearDown();
+	}
+
 	/**
 	 * @test
 	 */
 	public function itShouldConvert() {
 		$id          = 123;
 		$convertedId = 456;
-		$class = 'my-class';
-		$attribute = 'data-my-id';
-		$type = 'page';
+		$class       = 'my-class';
+		$attribute   = 'data-my-id';
+		$slug        = 'page';
 
 		$config = [
-			[ 'xpath' => '//*[contains(@class, "' . $class . '")]/@' . $attribute, 'type' => $type ],
+			[
+				'xpath' => '//*[contains(@class, "' . $class . '")]/@' . $attribute,
+				'slug'  => $slug,
+				'type'  => 'post',
+			],
 		];
 
 		$getBlock = function( $id ) use ( $class, $attribute ) {
@@ -36,13 +45,27 @@ class TestTagAttributes extends \OTGS_TestCase {
 		$block         = $getBlock( $id );
 		$expectedBlock = $getBlock( $convertedId );
 
-		\WP_Mock::userFunction( 'wpml_object_id_filter', [
-			'args'   => [ $id, $type ],
-			'return' => $convertedId,
-		] );
+		$this->mockConvertIds( $id, $convertedId, $slug );
 
 		$subject = new TagAttributes( $config );
 
 		$this->assertEquals( $expectedBlock, $subject->convert( $block ) );
+	}
+
+	private function mockConvertIds( $id, $convertedId, $slug ) {
+		global $sitepress;
+
+		$sitepress = $this->getMockBuilder( '\SitePress' )
+			->setMethods( [ 'is_display_as_translated_post_type' ] )
+			->disableOriginalConstructor()->getMock();
+
+		$sitepress->method( 'is_display_as_translated_post_type' )
+			->with( $slug )
+			->willReturn( false );
+
+		\WP_Mock::userFunction( 'wpml_object_id_filter', [
+			'args'   => [ $id, $slug ],
+			'return' => $convertedId,
+		] );
 	}
 }
